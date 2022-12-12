892a5e54a4067ff26cf73c466cd2cc00b6c97dfb
Viktor <viktor.johansson96@gmail.com>
2022-12-12 13:53:23 +0100

    Removed unused components and pages.

--- frontend/src/router/AppRoutes.tsx
+++ frontend/src/router/AppRoutes.tsx
@@ -1,39 +1,13 @@
 import React, { lazy, Suspense } from 'react';
 import { HashRouter, Route, Routes } from 'react-router-dom';
 
-import PageLoading from '@/components/common/PageLoading';
-import SubPage from '@/pages/admin/SubPage';
-import NotFound from '@/pages/NotFound';
-
-const HomeView = lazy(() => import('@/pages/HomeView'));
-const Admin = lazy(() => import('@/pages/Admin'));
-const Login = lazy(() => import('@/pages/Login'));
+import HomeView from '@/pages/HomeView';
 
 const AppRoutes = () => {
-  console.log('AppRoutes render...');
   return (
     <HashRouter>
       <Routes>
-        <Route
-          path={`/`}
-          element={
-            <Suspense fallback={<PageLoading />}>
-              <HomeView />
-            </Suspense>
-          }
-        />
-        <Route path={`/login`} element={<Login />} />
-        <Route
-          path={`/admin`}
-          element={
-            <Suspense fallback={<PageLoading />}>
-              <Admin />
-            </Suspense>
-          }
-        >
-          <Route path={'sub'} element={<SubPage />} />
-        </Route>
-        <Route path={'*'} element={<NotFound />} />
+        <Route path={`/`} element={<HomeView />} />
       </Routes>
     </HashRouter>
   );
