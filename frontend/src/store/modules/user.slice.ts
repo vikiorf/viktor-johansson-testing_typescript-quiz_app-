5e61b60d81019273aff6e214aac47ec6648c0ce7
Viktor <viktor.johansson96@gmail.com>
2022-12-13 22:13:28 +0100

    Added name to user-slice.

--- frontend/src/store/modules/user.slice.ts
+++ frontend/src/store/modules/user.slice.ts
@@ -3,15 +3,21 @@ import { RootState } from '../index';
 
 const { reducer: userReducer, actions } = createSlice({
   name: 'user',
-  initialState: { isCookiesConsentApproved: false },
+  initialState: { isCookiesConsentApproved: false, name: '' },
   reducers: {
     setCookieConsent(state, action: PayloadAction<boolean>) {
       state.isCookiesConsentApproved = action.payload;
+    },
+    setUserName(state, action: PayloadAction<string>) {
+      state.name = action.payload;
     },
   },
 });
 
 export const getStoredCookieConsent = (state: RootState) =>
   state.user.isCookiesConsentApproved;
+export const getStoredUserName = (state: RootState) => state.user.name;
+
 export const { setCookieConsent } = actions;
+export const { setUserName } = actions;
 export default userReducer;
