794af8dde1aa671819e81318478d964d1d84e8df
Viktor <viktor.johansson96@gmail.com>
2022-12-13 17:29:35 +0100

    Added CookiePrompt to HomeView.

--- frontend/src/pages/HomeView.tsx
+++ frontend/src/pages/HomeView.tsx
@@ -1,11 +1,17 @@
 import { FC } from 'react';
 
+import { useAppSelector } from '@/app/hooks';
+
 import ListItemComponent from '@/components/common/ListItemComponent';
 import ListComponent, { GapSizeEnum } from '@/components/common/ListComponent';
+import CookiePromptComponent from '@/components/Specific/CookiePromptComponent';
+import { getStoredCookieConsent } from '@/store/modules/user.slice';
 
 type IHomeView = {};
 
 const HomeView: FC<IHomeView> = () => {
+  const isCookiesConsentApproved = useAppSelector(getStoredCookieConsent);
+
   return (
     <div className="wrapper h-screen w-screen p-10 bg-primary-bg ">
       <h1
@@ -18,6 +24,7 @@ const HomeView: FC<IHomeView> = () => {
         <ListItemComponent>🙍‍♀️ Play alone</ListItemComponent>
         <ListItemComponent>💃 Play with other people</ListItemComponent>
       </ListComponent>
+      {!isCookiesConsentApproved && <CookiePromptComponent />}
     </div>
   );
 };
