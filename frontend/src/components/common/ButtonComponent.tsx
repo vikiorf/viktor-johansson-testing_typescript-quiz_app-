c8de8e3e1cbf5de716a1d89e0f44f2bd66827c51
Viktor <viktor.johansson96@gmail.com>
2022-12-18 00:37:04 +0100

    Updated ButtonComponent: Added sizes to ButtonComponent

--- frontend/src/components/common/ButtonComponent.tsx
+++ frontend/src/components/common/ButtonComponent.tsx
@@ -5,23 +5,41 @@ export enum ButtonStyleEnum {
   SECONDARY = 'bg-secondary-button-bg text-primary-color',
 }
 
+export enum ButtonSizeEnum {
+  MEDIUM = 'w-24 h-10',
+  LARGE = 'w-36 h-14 text-xl',
+}
+
 type IInputComponent = {
   buttonType?: 'button' | 'submit';
+  buttonSize?: ButtonSizeEnum;
   buttonStyle?: ButtonStyleEnum;
   label: string;
   onClick?: () => void;
+  extendCssClass?: string;
 };
 
 const ButtonComponent: FC<IInputComponent> = props => {
   const [buttonType, setButtonType] = useState('button');
   const [className, setClassName] = useState(
-    'placeholder-placeholder-color pl-1 h-10 rounded w-24 hover:cursor-pointer self-center justify-self-center ',
+    'placeholder-placeholder-color pl-1 rounded hover:cursor-pointer self-center justify-self-center ',
   );
 
   useEffect(() => {
+    let cssClasses = '';
     let buttonStyle = ButtonStyleEnum.PRIMARY;
+    let buttonSize = ButtonSizeEnum.MEDIUM;
+
     if (props.buttonStyle) buttonStyle = props.buttonStyle;
-    setClassName(className + buttonStyle);
+
+    if (props.extendCssClass) cssClasses += props.extendCssClass;
+
+    if (props.buttonSize === ButtonSizeEnum.LARGE) buttonSize = ButtonSizeEnum.LARGE;
+
+    cssClasses += ' ' + buttonSize;
+    cssClasses += ' ' + buttonStyle;
+
+    setClassName(className + cssClasses);
 
     if (props.buttonType) setButtonType(props.buttonType);
   }, []);
